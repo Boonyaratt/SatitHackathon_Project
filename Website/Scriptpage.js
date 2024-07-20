90ca989faa15c44f57fe8e33cdab4a34ffe7b9e1
Kanathuch Siribannakul <131222639+Waterforces@users.noreply.github.com>
2024-07-20 13:00:13 +0700

    Commits 5

--- Website/Scriptpage.js
+++ Website/Scriptpage.js
@@ -1,3 +1,6 @@
+var loginUser = ["Apple","abc"];
+var loginPass = ["Banna","123"];
+
 function gotoLearnPage() {
     window.location.href="LearnPage.html";
 }
@@ -11,4 +14,21 @@ function changeLanguage() {
     var url=window.location.href;
     var baseUrl = url.split("?")[0];
     window.location.href=baseUrl+"?lang="+selectedLanguageanguage;
+}
+
+function  gotoLoginPage() {
+    window.location.href="LoginPage.html";
+}
+
+function checkLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    for (var i = 0; i < loginUser.length; i++) {
+        if (loginUser[i] == username && loginPass[i] == password) {
+            alert("LogIn Sucessful 😍😍😍");
+            window.location.href="HomePage.html"; //Not working
+            return;
+        }
+    }
+    alert("Wrong Username or Password, Try again! 😅😅😅");
 }
