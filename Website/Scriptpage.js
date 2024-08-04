eaa84bee2cb21fe40faafe781879ef4a5808f4f3
Boonyaratt <Boonyaratt1990@gmail.com>
2024-08-04 11:24:40 +0700

    Update and decorate css and html files
    Update each css page using Chat gpt

--- Website/Scriptpage.js
+++ Website/Scriptpage.js
@@ -1,37 +1,56 @@
-function gotoLearnPage() {
-    window.location.href="LearnPage.html";
-}
-
-function gotoHomePage() {
-    window.location.href="HomePage.html";
-}
-
+//Define URLs 
+var LearnPageURL = "LearnPage.html";
+var HomePageURL = "HomePage.html";
+var BefHomePageURL = "BefHomePage.html";
+var LoginPageURL = "LoginPage.html";
+var UserAccount = [
+    { username: "admin", password: "admin" },
+    { username: "test01", password: "betatest" },
+    { username: "1", password: "1" }
+];
+//Function goto another website
 function gotoBefHomePage() {
-    window.location.href="BefHomePage.html";
+    window.location.href = BefHomePageURL;
 }
-
-function changeLanguage() {
-    var selectedLanguage = document.getElementById("language").value;
-    var url=window.location.href;
-    var baseUrl = url.split("?")[0];
-    window.location.href=baseUrl+"?lang="+selectedLanguageanguage;
+function gotoHomePage() {
+    window.location.href = HomePageURL;
 }
-
-function  gotoLoginPage() {
-    window.location.href="LoginPage.html";
+function gotoLoginPage() {
+    window.location.href = LoginPageURL;
 }
-
-var loginUser = ["Apple","abc"];
-var loginPass = ["Banna","123"];
-
+function gotoLearnPage() {
+    window.location.href = LearnPageURL;
+}
+function gotoOthersPage() {
+    window.location.href = "OthersPage.html";
+}
+function showScore() {
+    var show = document.getElementById("scorePage");
+    var ask = document.getElementById("askPage");
+    if (show) {
+        ask.style.visibility = "hidden";
+        show.style.visibility = "visible";
+    }
+}
+function showAsk() {
+    var ask = document.getElementById("askPage");
+    var show = document.getElementById("scorePage");
+    if (ask) {
+        show.style.visibility = "hidden";
+        ask.style.visibility = "visible";
+    }
+}
+//check loginAccount
 function checkLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    for (var i = 0; i < loginUser.length; i++) {
-        if (loginUser[i] == username && loginPass[i] == password) {
+    for (var i = 0; i < UserAccount.length; i++) {
+        if (UserAccount[i].username == username && UserAccount[i].password == password) {
             alert("LogIn Sucessful 😍😍😍");
-            window.location.assign("HomePage.html") //Not working
-            return ;
+            setTimeout(function () {
+                window.location.assign("HomePage.html");
+            }, 0);
+            return;
         }
     }
     alert("Invalid username or password, Try again! 😅😅😅");
